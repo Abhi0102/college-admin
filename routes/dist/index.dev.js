4985e3a6dee793d7be6454c7ebb2b5f865b051f5
Abhi0102 <67204033+Abhi0102@users.noreply.github.com>
2022-02-07 20:40:28 +0530

    Routes

--- routes/dist/index.dev.js
+++ routes/dist/index.dev.js
@@ -7,5 +7,13 @@ var router = express.Router();
 var pdf = require("../controllers/api/v1/idCardAPI");
 
 router.use("/", require("./api/v1/index"));
-router.get("/hey", pdf.pdfCreate);
+router.get("/", function (req, res) {
+  return res.status(200).json({
+    status: true,
+    data: {
+      a: "Hello",
+      b: "Yolo"
+    }
+  });
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ const pdf = require("../controllers/api/v1/idCardAPI");
 
 router.use("/", require("./api/v1/index"));
 
-router.get("/hey", pdf.pdfCreate);
+router.get("/", function (req, res) {
+  return res.status(200).json({
+    status: true,
+    data: {
+      a: "Hello",
+      b: "Yolo",
+    },
+  });
+});
 
 module.exports = router;
