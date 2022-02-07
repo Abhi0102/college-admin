db100237c289be248ea4fae90dede49020f1c0f3
Abhi0102 <67204033+Abhi0102@users.noreply.github.com>
2022-02-07 20:15:42 +0530

    Routes Improve

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -12,25 +12,11 @@ var passport = require("passport");
 
 var jwtPassport = require("./config/passport-jwt");
 
-var path = require("path"); // const port = process.env.port || 4000;
-
+var path = require("path");
 
 var app = express();
-app.use(express.urlencoded()); // app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PATCH");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
-
-app.use("/", routes);
+app.use(express.urlencoded());
+app.use("/api", routes);
 app.use(express["static"]("client/build"));
 app.get("*", function (req, res) {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
--- index.js
+++ index.js
@@ -5,26 +5,11 @@ const db = require("./config/mongoose");
 const passport = require("passport");
 const jwtPassport = require("./config/passport-jwt");
 const path = require("path");
-// const port = process.env.port || 4000;
 const app = express();
 
 app.use(express.urlencoded());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PATCH");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
-
-app.use("/", routes);
+app.use("/api", routes);
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
